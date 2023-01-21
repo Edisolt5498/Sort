1f1711d709425da4a143fc885e151c539699b106
Edisolt5498 <edisolt5498@gmail.com>
2023-01-21 10:15:53 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws FileNotFoundException {
+        int[] first = {3, 20, 21, 200, 300, 400, 500};
+        int[] second = {2, 5, 7, 9, 12, 13, 500, 700, 90000};
+
+        System.out.println(Arrays.toString(Sort.mergeArray(first, second)));
     }
 }
+
+
+
--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Sort {
+public class Sort {
+    public static int[] mergeArray (int[] a, int[] b) {
+
+        if (a.length == 0) return b;
+        else if (b.length == 0) return a;
+
+        int [] sorted = new int[a.length + b.length];
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        boolean firstBigger = false;
+
+        if (true) {
+            while (j < b.length && i < a.length) {
+                if (a[i] < b[j]) {
+                    sorted[count] = a[i];
+                    count++;
+                    i++;
+                    firstBigger = false;
+                } else {
+                    sorted[count] = b[j];
+                    count++;
+                    j++;
+                    firstBigger = true;
+                }
+            }
+            if (firstBigger) {
+                for (int k = i; k < a.length; k++) {
+                    sorted[count] = a[k];
+                    count++;
+                }
+            } else if (!firstBigger) {
+                for (int k = j; k < b.length; k++) {
+                    sorted[count] = b[k];
+                    count++;
+                }
+            }
+        }
+        return sorted;
+    }
 }
